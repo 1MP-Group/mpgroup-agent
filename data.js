949952b12f953f17564026d3371b7bae6c9e4e0f
1MP-Group <info@mpgroup.org>
2026-03-09 15:02:24 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,138 +1,128 @@
 const agents = [
-    {
-      "name": "Alena Marchant",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/alena-marchant/",
-      "states": ["Alabama", "Arkansas", "Arizona", "California", "Colorado", "Delaware", "Washington D.C", "Florida", "Georgia", "Illinois", "Indiana", "Kentucky", "Maryland", "Michigan", "Minnesota", "Missouri", "New York", "North Carolina", "North Dakota", "Ohio", "Pennsylvania", "South Carolina", "South Dakota", "Tennessee", "Texas", "Virginia", "West Virginia"]
-    },
-
-   {
-      "name": "Amy Noone",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/contact-licensed-medicare-insurance-agent-amy-noone-now/",
-      "states": ["Delaware", "Florida", "Illinois", "Maryland", "New Jersey", "North Carolina", "Pennsylvania", "South Carolina", "Texas", "Washington D.C", "Virginia"]
-    },
-
-    {
-      "name": "Charley Spiridopoulos",
-      "agent_email": "cspiro@verizon.net",
-      "agent_page": "",
-      "states": ["Virginia","Maryland","Washington D.C."]
-    },
-    {
-      "name": "Chris Phillips",
-      "agent_email": "chris@mpgroup.org",
-      "agent_page": "",
-      "states": ["Virginia","Maryland","Washington D.C.","Delaware","Florida","Georgia","Pennsylvania","Texas","West Virginia"]
-    },
-
-   {
-      "name": "Claire O'Halloran",
-      "agent_email": "claire@mpgroup.org",
-      "agent_page": "",
-      "states": ["Virginia","Maryland","Washington D.C.","Alabama"]
-    },
-
-    
-    {
-      "name": "Dan Csaky",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/dan-csaky/",
-      "states": ["North Carolina","Georgia","Ohio","Virginia","Washington D.C.","Wisconsin"]
-    },
-    {
-      "name": "Darren Harvey",
-      "agent_email": "dharvey@capitalgroupbenefits.com",
-      "agent_page": "",
-      "states": ["Virginia","Maryland","Washington D.C.","Florida","Arizona","Colorado","Kansas","Missouri"]
-    },
-    {
-      "name": "Emily O'Halloran",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/emily-ohalloran/",
-      "states": ["Alabama", "Arizona", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Maryland", "Massachusetts", "Minnesota", "Missouri", "New Jersey", "New Mexico", "North Carolina", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Washington D.C.", "West Virginia", "Wisconsin"]
-    },
-    {
-      "name": "Gabe Pilato",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/gabe-pilato/",
-      "states": ["Alabama","Colorado","Georgia", "Minnesota", "South Carolina"]
-    },
-    {
-      "name": "Gary Cox",
-      "agent_email": "gary@mpgroup.org",
-      "agent_page": "",
-      "states": ["Virginia","Maryland","Washington D.C.","Pennsylvania"]
-    },
-    {
-      "name": "Jeff LeSourd",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/jeff-lesourd/",
-      "states": ["Virginia", "Maryland", "West Virginia", "North Carolina", "Florida", "Ohio", "Pennsylvania", "Wyoming"]
-    },
-    {
-      "name": "John Norce",
-      "agent_email": "john@mpgroup.org",
-      "agent_page": "",
-      "states": ["Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Iowa", "Illinois", "Indiana", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Vermont", "Washington D.C.", "West Virginia", "Wyoming"
-]
-    },
-    {
-      "name": "Karin Cantrell",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/karin-cantwell/",
-      "states": ["Delaware", "Florida", "Maryland", "New York", "North Carolina", "Pennsylvania", "Virginia", "Washington D.C."
-"]
-    },
-    {
-      "name": "Kevin Chaikin",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/kevin-chaikin/",
-      "states": ["Alabama","Arizona","California","Colorado","Connecticut","Washington D.C.","Delaware","Florida","Georgia","Iowa","Illinois","Indiana","Louisiana","Kentucky","Massachusetts","Maryland","Maine","Michigan","Minnesota","North Carolina","Nebraska","New Hampshire","New Jersey","New Mexico","New York","Nevada","Ohio","Oregon","Pennsylvania","South Carolina","Tennessee","Texas","Virginia","West Virginia","Wyoming"]
-    },
-
   {
-      "name": "Lisa Kohrt",
-      "agent_email": "lisa@mpgroup.org",
-      "agent_page": "",
-      "states": ["Alabama", "Delaware", "Florida", "Georgia", "Maryland", "Michigan", "North Carolina", "Ohio", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Washington D.C.", "West Virginia"
-]
-    },
-
-    {
-      "name": "Mike Hamric",
-      "agent_email": "mhamric@protonmail.com",
-      "agent_page": "",
-      "states": ["Virginia", "Maryland", "West Virginia"]
-    },
-    {
-      "name": "Paul Carswell",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/paul-carswell-2/",
-      "states": ["Alabama", "Washington D.C.", "Delaware", "Florida", "Georgia", "Maryland", "Michigan", "North Carolina", "New Mexico", "New Jersey", "Ohio", "Oklahoma", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia"]
-    },
-    {
-      "name": "Riley O'Halloran",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/riley-ohalloran/",
-      "states": ["Maryland", "Virginia", "North Carolina", "South Carolina", "New York"]
-    },
-    {
-      "name": "Sam Koo",
-      "agent_email": "bonsamkoo@gmail.com",
-      "agent_page": "",
-      "states": ["Virginia", "Maryland", "Pennsylvania", "New York", "New Jersey", "Florida", "California", "South Carolina", "Alabama", "Georgia"]
-    },
-     {
-      "name": "Kathleen O'Halloran",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/kathleen-ohalloran/",
-      "states": ["Washington D.C.", "Florida", "Maryland", "Michigan", "North Carolina", "South Carolina", "Virginia", "West Virginia"]
-    },
-    {
-      "name": "Lisa Scott",
-      "agent_email": "",
-      "agent_page": "https://mpgroup.org/lisa-scott/",
-      "states": ["Washington D.C", "Florida", "Maryland", "New York", "Ohio", "Pennsylvania", "South Carolina", "Virginia"]
-    }
+    "name": "Alena Marchant",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/alena-marchant/",
+    "states": ["Alabama", "Arkansas", "Arizona", "California", "Colorado", "Delaware", "Washington D.C", "Florida", "Georgia", "Illinois", "Indiana", "Kentucky", "Maryland", "Michigan", "Minnesota", "Missouri", "New York", "North Carolina", "North Dakota", "Ohio", "Pennsylvania", "South Carolina", "South Dakota", "Tennessee", "Texas", "Virginia", "West Virginia"]
+  },
+  {
+    "name": "Amy Noone",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/contact-licensed-medicare-insurance-agent-amy-noone-now/",
+    "states": ["Delaware", "Florida", "Illinois", "Maryland", "New Jersey", "North Carolina", "Pennsylvania", "South Carolina", "Texas", "Washington D.C", "Virginia"]
+  },
+  {
+    "name": "Charley Spiridopoulos",
+    "agent_email": "cspiro@verizon.net",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Washington D.C."]
+  },
+  {
+    "name": "Chris Phillips",
+    "agent_email": "chris@mpgroup.org",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Washington D.C.", "Delaware", "Florida", "Georgia", "Pennsylvania", "Texas", "West Virginia"]
+  },
+  {
+    "name": "Claire O'Halloran",
+    "agent_email": "claire@mpgroup.org",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Washington D.C.", "Alabama"]
+  },
+  {
+    "name": "Dan Csaky",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/dan-csaky/",
+    "states": ["North Carolina", "Georgia", "Ohio", "Virginia", "Washington D.C.", "Wisconsin"]
+  },
+  {
+    "name": "Darren Harvey",
+    "agent_email": "dharvey@capitalgroupbenefits.com",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Washington D.C.", "Florida", "Arizona", "Colorado", "Kansas", "Missouri"]
+  },
+  {
+    "name": "Emily O'Halloran",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/emily-ohalloran/",
+    "states": ["Alabama", "Arizona", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Maryland", "Massachusetts", "Minnesota", "Missouri", "New Jersey", "New Mexico", "North Carolina", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Washington D.C.", "West Virginia", "Wisconsin"]
+  },
+  {
+    "name": "Gabe Pilato",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/gabe-pilato/",
+    "states": ["Alabama", "Colorado", "Georgia", "Minnesota", "South Carolina"]
+  },
+  {
+    "name": "Gary Cox",
+    "agent_email": "gary@mpgroup.org",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Washington D.C.", "Pennsylvania"]
+  },
+  {
+    "name": "Jeff LeSourd",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/jeff-lesourd/",
+    "states": ["Virginia", "Maryland", "West Virginia", "North Carolina", "Florida", "Ohio", "Pennsylvania", "Wyoming"]
+  },
+  {
+    "name": "John Norce",
+    "agent_email": "john@mpgroup.org",
+    "agent_page": "",
+    "states": ["Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Iowa", "Illinois", "Indiana", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Vermont", "Washington D.C.", "West Virginia", "Wyoming"]
+  },
+  {
+    "name": "Karin Cantrell",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/karin-cantwell/",
+    "states": ["Delaware", "Florida", "Maryland", "New York", "North Carolina", "Pennsylvania", "Virginia", "Washington D.C."]
+  },
+  {
+    "name": "Kevin Chaikin",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/kevin-chaikin/",
+    "states": ["Alabama", "Arizona", "California", "Colorado", "Connecticut", "Washington D.C.", "Delaware", "Florida", "Georgia", "Iowa", "Illinois", "Indiana", "Louisiana", "Kentucky", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "North Carolina", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "New York", "Nevada", "Ohio", "Oregon", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "West Virginia", "Wyoming"]
+  },
+  {
+    "name": "Lisa Kohrt",
+    "agent_email": "lisa@mpgroup.org",
+    "agent_page": "",
+    "states": ["Alabama", "Delaware", "Florida", "Georgia", "Maryland", "Michigan", "North Carolina", "Ohio", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia", "Washington D.C.", "West Virginia"]
+  },
+  {
+    "name": "Mike Hamric",
+    "agent_email": "mhamric@protonmail.com",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "West Virginia"]
+  },
+  {
+    "name": "Paul Carswell",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/paul-carswell-2/",
+    "states": ["Alabama", "Washington D.C.", "Delaware", "Florida", "Georgia", "Maryland", "Michigan", "North Carolina", "New Mexico", "New Jersey", "Ohio", "Oklahoma", "Pennsylvania", "South Carolina", "Tennessee", "Texas", "Virginia"]
+  },
+  {
+    "name": "Riley O'Halloran",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/riley-ohalloran/",
+    "states": ["Maryland", "Virginia", "North Carolina", "South Carolina", "New York"]
+  },
+  {
+    "name": "Sam Koo",
+    "agent_email": "bonsamkoo@gmail.com",
+    "agent_page": "",
+    "states": ["Virginia", "Maryland", "Pennsylvania", "New York", "New Jersey", "Florida", "California", "South Carolina", "Alabama", "Georgia"]
+  },
+  {
+    "name": "Kathleen O'Halloran",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/kathleen-ohalloran/",
+    "states": ["Washington D.C.", "Florida", "Maryland", "Michigan", "North Carolina", "South Carolina", "Virginia", "West Virginia"]
+  },
+  {
+    "name": "Lisa Scott",
+    "agent_email": "",
+    "agent_page": "https://mpgroup.org/lisa-scott/",
+    "states": ["Washington D.C", "Florida", "Maryland", "New York", "Ohio", "Pennsylvania", "South Carolina", "Virginia"]
+  }
 ];
